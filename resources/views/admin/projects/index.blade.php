d62f57955c54d8b8c84ddb7e591ffba7d9b6c10e
MarBozzz <mbozzuto@libero.it>
2023-01-18 18:18:31 +0100

    [added] working admin.view

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,9 +14,9 @@
             <div class="card" style="width: 18rem;">
                 <img src="{{$project->cover_image}}" class="card-img-top" alt="{{$project->name}}">
                 <div class="card-body">
-                <h5 class="card-title">{{$project->name}}</h5>
+                <h3 class="card-title py-3 text-center">{{$project->name}}</h5>
                 <p class="card-text">{{$project->summary}}</p>
-                <a href="#" class="btn btn-primary">Details</a>
+                <a class="btn btn-primary" href="{{route('admin.projects.show', $project)}}" title="show">See Details >></a>
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+@section('title')
+    | Admin
+@endsection
+
+@section('content')
+<div class="container d-flex justify-content-center align-items-center flex-column">
+    <div class="card my-5" style="width: 18rem;">
+        <img src="{{$project->cover_image}}" class="card-img-top" alt="{{$project->name}}">
+        <div class="card-body">
+        <h3 class="card-title text-center py-3">{{$project->name}}</h5>
+        <h5 class="card-title">Client name: {{$project->client_name}}</h5>
+        <p class="card-text py-3">Summary{{$project->summary}}</p>
+        </div>
+    </div>
+    <a class="btn btn-primary" href="{{route('admin.projects.index')}}" >TORNA A PROJECTS</a>
+
+</div>
+@endsection
